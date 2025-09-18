26609d30393caa8fcae172f1cf74b4286fe5c4f4
Bob Zheng <bob.zheng6042@gmail.com>
2025-09-18 13:37:44 -0400

    almost done

--- app/(Kambaz)/Courses/[cid]/Home/Status.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/Status.tsx
@@ -1,9 +1,18 @@
 export default function CourseStatus() {
   return (
-    <div id="wd-course-status">
+    <div id="wd-course-status" style={{ display: 'flex', flexDirection: 'column' }}>
       <h2>Course Status</h2>
-      <button>Unpublish</button> <button>Publish</button>
-      {/* Complete on your own */}
+      <div>
+        <button>Unpublish</button> 
+        <button>Publish</button>
+      </div>
+      <br/>
+      <button>Important Existing Content</button> 
+      <button>Import from Commons</button>
+      <button>Choose Home Page</button> 
+      <button>View Course Stream</button>
+      <button>New Announcement</button> 
+      <button>New Analytics</button>
       <button>View Course Notifications</button>
     </div>
   );
--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -1,10 +1,16 @@
 export default function Modules() {
   return (
     <div>
-      {/* Implement Collapse All button, View Progress button, etc. */}
+      <button id="wd-collapse-all">Collapse All</button>
+      <button id="wd-view-progress">View Progress</button>
+      <select title="Publish All" id="wd-publish" defaultValue="Publish All">
+        <option value="Publish All">Publish All</option>
+        <option value="Unpublish All">Unpublish All</option>
+      </select>
+      <button id="wd-add-module">+ Module</button>
       <ul id="wd-modules">
         <li className="wd-module">
-          <div className="wd-title">Week 1</div>
+          <div className="wd-title">Week 1, Lecture 1 - Course Introduction, Syllabus, Agenda</div>
           <ul className="wd-lessons">
             <li className="wd-lesson">
               <span className="wd-title">LEARNING OBJECTIVES</span>
@@ -15,13 +21,48 @@ export default function Modules() {
                 </li>
               </ul>
             </li>
+            <li className="wd-lesson">
+              <span className="wd-title">READINGS</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Full stack dev - Ch 1  
+                </li>
+                <li className="wd-content-item">
+                  Full stack dev - Ch 2
+                </li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Week 1 Slides
+                </li>
+              </ul>
+            </li>
           </ul>
         </li>
         <li className="wd-module">
-          <div className="wd-title">Week 2</div>
-        </li>
-        <li className="wd-module">
-          <div className="wd-title">Week 3</div>
+          <div className="wd-title">Week 1, Lecture 2 - Formatting User Interfaces using HTML</div>
+          <ul className="wd-lessons">
+            <li className="wd-lesson">
+              <span className="wd-title">LEARNING OBJECTIVES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Learn how to create user interfaces with HTML</li>
+                <li className="wd-content-item">
+                  Deploy assignment to Netlify
+                </li>
+              </ul>
+            </li>
+            <li className="wd-lesson">
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">
+                  Week 1 Slides
+                </li>
+              </ul>
+            </li>
+          </ul>
         </li>
       </ul>
     </div>
